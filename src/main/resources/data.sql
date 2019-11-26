57ceffcff624ee21e3f35b75bd54ad6b6011169e
craigb <BrandonCraig@rentalcars.com>
2019-11-26 14:46:08 +0000

    Added employee_info db

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS employee_info (
+                              id VARCHAR(16)  PRIMARY KEY NOT NULL,
+                              first_name VARCHAR(250) NOT NULL,
+                              last_name VARCHAR(250) NOT NULL,
+                              email VARCHAR(250) NOT NULL,
+                              mobile_number VARCHAR(250) NOT NULL
+);
+
