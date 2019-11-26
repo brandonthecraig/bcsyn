b420ab2de03f9fb09f5e932a1c1e53b8f4ea4f88
craigb <BrandonCraig@rentalcars.com>
2019-11-26 16:13:06 +0000

    Controller Tests

--- src/test/java/com/firstcateringlimited/bowsapi/controllers/BowsApiControllerTest.java
+++ src/test/java/com/firstcateringlimited/bowsapi/controllers/BowsApiControllerTest.java
@@ -1,16 +1,10 @@
 package com.firstcateringlimited.bowsapi.controllers;
 
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.firstcateringlimited.bowsapi.responses.EndSessionResponse;
+import com.firstcateringlimited.bowsapi.responses.RegisteredCheckResponse;
 import com.firstcateringlimited.bowsapi.services.BowsApiService;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -36,11 +30,21 @@ public class BowsApiControllerTest {
     BowsApiService bowsApiServiceMock;
 
     @Test
-    public void testEndSessionApiReturn() throws Exception {
+    public void testEndSessionApiReturnsExpectedResultes() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/endsession").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.endSession").value("true"))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.exitMessage").value("You are now exiting the session. Goodbye"));
+    }
+
+    @Test
+    public void testCheckIfRegisteredApiReturnHasNeededFields() throws Exception {
+        Mockito.when(bowsApiServiceMock.checkIfRegistered("123")).thenReturn(new RegisteredCheckResponse());
+
+        mvc.perform(MockMvcRequestBuilders.get("/registeredcheck/123").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.registrationVerified").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.welcomeMessage").isEmpty());
     }
 
 
